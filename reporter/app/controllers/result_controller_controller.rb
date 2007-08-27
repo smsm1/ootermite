c7bf2e263c55544f9db393ee2b39b20ed1795885
Michael Leibowitz <mikeleib@users.noreply.github.com>
2007-08-27 18:15:59 +0000

    import the proto-rails app

---
+++ reporter/app/controllers/result_controller_controller.rb
@@ -0,0 +1,25 @@
+class ResultControllerController < ApplicationController
+
+  # only create is valid
+  # FIXME: we need to catch ActiveRecord::SerializationTypeMismatch
+  # and do the right thing
+  def create
+    @result= Result.new(params[:result])
+    if @result.save
+      head :created
+    else
+      render :xml => @result.errors.to_xml
+    end
+  end
+
+  def new ; no ; end
+  def show ; no ; end
+  def edit ; no ; end
+  def update ; no ; end
+  def destroy ; no ; end
+
+  private
+  def no
+    head :method_not_allowed
+  end
+end
---
+++ reporter/app/helpers/result_controller_helper.rb
@@ -0,0 +1,2 @@
+module ResultControllerHelper
+end
