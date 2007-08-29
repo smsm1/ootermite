8085872b07227d487ba1d39fb0fb80eb0394ee3b
ikeleib <ikeleib@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2007-08-29 20:55:03 +0000

    Some additional tests to show that it works

--- trunk/reporter/test/functional/data_controller_test.rb
+++ trunk/reporter/test/functional/data_controller_test.rb
@@ -27,7 +27,42 @@ class DataControllerTest < Test::Unit::TestCase
   def test_sub_hashes
     post :create, :data => {}
     assert_response :ok
-    # FIXME: test the tags
+    assert_tag :tag => 'error', :content => "build cannot be null"
+    assert_tag :tag => 'error', :content => "data_item cannot be null"
+  end
+
+  def test_create_correct_parts
+    post :create, :data => {
+      :build => {
+        :pws => 'pws1',
+        :cws => 'cws1',
+        :builder => 'builder1',
+        :host => 'host1',
+        :build_number => 1},
+      :data_item => {
+        :data_type => 'type1',
+        :data => { 'value' => 1 }
+      }}
+      assert_response :created
+    build= Build.find_by_builder('builder1')
+    assert build
+    data_item= DataItem.find_by_data_type('type1')
+    assert data_item
+    assert_equal build.id, data_item.build_id
+    
+    post :create, :data => {
+      :build => {
+        :pws => 'pws1',
+        :cws => 'cws1',
+        :builder => 'builder1',
+        :host => 'host1',
+        :build_number => 1},
+      :data_item => {
+        :data_type => 'type2',
+        :data => { 'value' => 1 }
+      }}
+    assert_response :created
+    assert_equal 2, build.data_items.count
   end
 
 end
