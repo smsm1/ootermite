d783a0dd899f305577f916af752e3898df86056a
Shaun McDonald <mail@shaunmcdonald.me.uk>
2007-10-06 23:34:22 +0000

    now adding the try for the exception handling.

--- buildbot/buildmaster/OOtinder.py
+++ buildbot/buildmaster/OOtinder.py
@@ -71,10 +71,11 @@ class OOTinderboxMailNotifier(tinderbox.TinderboxMailNotifier):
                   text += "%s END\n" % t
             # if the build finished...
             else:
+                try:
                   if (build.getProperty("install_set")) and (results == SUCCESS):
                         text += "TinderboxPrint: <a href=\"http://termite.go-oo.org/install_sets/%s-%s-install_set.zip\">Install Set</a>\n" % (build.getProperty("buildername"), build.getProperty("buildnumber"))
                   # Do it slightly differently if we don't have an install_set
-                  except KeyError:
+                except KeyError:
                         if (results == SUCCESS):
                                 text += "TinderboxPrint: No install set was produced"
                                 
