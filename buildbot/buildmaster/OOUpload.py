2836ed33601f6bd02e8701291514e0a1b11146cb
Shaun McDonald <mail@shaunmcdonald.me.uk>
2007-10-13 20:28:55 +0000

    adding the branch to the upload filename and using grey for the colour of the skipped installset

--- buildbot/buildmaster/OOUpload.py
+++ buildbot/buildmaster/OOUpload.py
@@ -10,16 +10,17 @@ class MyUpload(transfer.FileUpload):
               
       def start(self):
             bname = self.getProperty("buildername")
-            bnumber = self.getProperty("buildnumber")            
-            self.masterdest = "/home/buildmaster/buildmaster/install_sets/%s-%s-install_set.zip" % (bname, bnumber)
-            url = "http://termite.go-oo.org/install_sets/%s-%s-install_set.zip" % (bname, bnumber)
+            bnumber = self.getProperty("buildnumber")
+            bbranch = self.getProperty("branch")            
+            self.masterdest = "/home/buildmaster/buildmaster/install_sets/%s-%s-%s-install_set.zip" % (bname, bnumber, bbranch)
+            url = "http://termite.go-oo.org/install_sets/%s-%s-%s-install_set.zip" % (bname, bnumber, bbranch)
             if self.getProperty("install_set"):
                   self.addURL("Install Set", url)
                   return transfer.FileUpload.start(self)
             else:
                   #self.timer = reactor.callLater(1, self.done)
                   self.step_status.setText(['Install Set:', 'Skipped'])
-                  self.step_status.setColor("green")
+                  self.step_status.setColor("grey")
                   return BuildStep.finished(self, SKIPPED)
 
       def finish(self, result):
