bb0fa6b972c7c4e2c6a629fb06d7a5f13bbbb6b8
cahadras <cahadras@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2008-08-19 10:08:38 +0000

    Delete reporter module because it is not under active development.

--- reporter/app/models/data_item.rb
+++
@@ -1,9 +0,0 @@
-class DataItem < ActiveRecord::Base
-  belongs_to :build
-  validates_presence_of :data_type, :build_id
-  serialize :data, Hash
-  
-  validates_each :data do |rec, attr, value|
-    rec.errors.add("data", "is not a Hash") unless value.is_a? Hash
-  end
-end
