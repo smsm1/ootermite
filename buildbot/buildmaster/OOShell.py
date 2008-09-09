d07070d28dca6d19f049ca329b0e571a58ef240f
cahadras <cahadras@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2008-09-09 09:45:41 +0000

    Smoketest is now skipped and not failed.

--- buildbot/buildmaster/OOShell.py
+++ buildbot/buildmaster/OOShell.py
@@ -41,6 +41,10 @@ class OOShellCommand(ShellCommand):
                 self.build.buildFinished(['slave rejected CWS', 'CWS problem'], 'grey', SKIPPED)
             return SKIPPED
 
+        if cmd.rc != 0:
+            if self.describe(False) == ['Smoketest']:
+                return SKIPPED
+
         return ShellCommand.evaluateCommand(self, cmd)
 
     def getText(self, cmd, results):
