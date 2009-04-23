248778cd7ad54a0c2745a18851921fb49076a9b1
cahadras <cahadras@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2009-04-23 12:38:29 +0000

    Fix syntax error.

--- buildbot/master/EISStatus.py
+++ buildbot/master/EISStatus.py
@@ -92,7 +92,7 @@ class EISStatusReceiver(base.StatusReceiverMultiService):
             print("Exception occurred in submitTestResult: %s" % sys.exc_info()[0])
         return
 
-    def submitMWSTestResult(self, branch, builderName, resultPage, statusName)
+    def submitMWSTestResult(self, branch, builderName, resultPage, statusName):
         try:
             soap = self.getSOAP()
             soap.submitTestResultMWS(branch[:6], branch[7:], "Buildbot", builderName, resultPage, statusName)
