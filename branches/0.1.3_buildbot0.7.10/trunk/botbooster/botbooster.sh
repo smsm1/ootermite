46ca907e80414fa2e35e132ca601c93d91ba16b9
cahadras <cahadras@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2009-08-21 06:53:12 +0000

    not trunk, only its content

--- branches/0.1.3_buildbot0.7.10/trunk/botbooster/botbooster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BOTBOOSTER_ADMIN=<adminmail>
-WORKDIR=~/botbooster/
-
-cd $WORKDIR
-java -cp botbooster.jar:soap.jar:mailapi.jar:activation.jar:jaxser-0.1.jar botbooster.Main "ready for QA"
-cat "bot.log" | mail -s "BotBooster Status Report" $BOTBOOSTER_ADMIN
