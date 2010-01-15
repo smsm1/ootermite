1d7f6a99f6ace81da0358a72fa345723511779a9
gregor.hartmann@sun.com <gregor.hartmann@sun.com@6dbbfc30-b238-0410-9765-9185e0d3aec2>
2010-01-15 10:26:23 +0000

    implement new option ccUsePrivateSymlinks

--- buildbot/slave/ccsetup.sh
+++ buildbot/slave/ccsetup.sh
@@ -18,10 +18,39 @@ if [ -n "$ccdatapath" ]; then
           echo please set environment variables \$CXX and \$CC
           exit 1
         fi
-        if [ -L "$CXX" -a -L "$CC" ]; then
-          if [ `ls -l "$CXX" "$CC" | grep ccache | wc -l` -eq 2 ]; then
-            echo Using symlinked ccache
+#TODO handle CC and CXX without full path
+        if [ -L "$CXX" -a -L "$CC" -a `ls -l "$CXX" "$CC" 2>&1| grep ccache | wc -l` -eq 2 ]; then
+          echo Using symlinked ccache from system
+        elif [ "$ccUsePrivateSymlinks" == "TRUE" ]; then
+          ccbase=`basename $CC`
+          cxxbase=`basename $CXX`
+          mydir=`pwd`
+
+          if [ ! -d ../ccacheSymLinks ]; then
+            # create private symlinks
+            mkdir ../ccacheSymLinks
+            cd ../ccacheSymLinks
+            ln -s `which ccache` $ccbase
+            ln -s `which ccache` $cxxbase
+            basedir=`pwd`
+            cd $mydir
+            # check if compilers are found in PATHa
+            if [ $CC != `which $ccbase` -a $CC != $ccbase ]; then
+              echo "$CC has to be found as $ccbase in PATH"
+              exit 1
+            fi
+            if [ $CXX != `which $cxxbase` -a $CXX != $cxxbase ]; then
+              echo "$CXX has to be found as $cxxbase in PATH"
+              exit 1
+            fi
+          else
+            cd ../ccacheSymLinks
+            basedir=`pwd`
+            cd $mydir
           fi
+          # set new variables
+          CC=$basedir/$ccbase
+          CXX=$basedir/$cxxbase
         else
           if [ -n "$WRAPCMD" ]; then
             export CC="$WRAPCMD ccache $CC"
